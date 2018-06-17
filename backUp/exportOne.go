93d9c8fd6a811fbebfda0a23103e6994aaea22e9
zhoutk <zhoutk@189.cn>
2018-06-17 17:49:18 +0800

    writ to file extract func.

--- backUp/exportOne.go
+++ backUp/exportOne.go
@@ -8,28 +8,39 @@ import (
 )
 
 func ExportOne(fields DbConnFields, workDir string) {
-	var name string
+	var fileName string
 	if fields.fileAlias == "" {
-		name = workDir + fields.dbName + "-" + time.Now().Format("2006-01-02") + ".sql"
+		fileName = workDir + fields.dbName + "-" + time.Now().Format("2006-01-02") + ".sql"
 	}else{
-		name = workDir + fields.fileAlias + "-" + time.Now().Format("2006-01-02") + ".sql"
+		fileName = workDir + fields.fileAlias + "-" + time.Now().Format("2006-01-02") + ".sql"
 	}
-	fileObj, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
-	if err != nil {
-		fmt.Println("Failed to open the file", err.Error())
-		os.Exit(2)
-	}
-	defer fileObj.Close()
-	var content string
-	content = "/*   Mysql export" +
+
+	content := "/*   Mysql export" +
 		"\n\n		Host: " + fields.dbHost +
 		"\n\n		Port: " + strconv.Itoa(fields.dbPort) +
 		"\n\n		DataBase: " + fields.dbName +
 		"\n\n		Date: " + time.Now().Format("2006-01-02 15:04:05") +
 		"\n\n		Author: zhoutk@189.cn" +
-		"\n\n		Copyright: tolw-2018" +
+		"\n\n		Copyright: tlwl-2018" +
 		"\n*/\n\n"
+	writeToFile(fileName, content, false)
+	writeToFile(fileName, "SET FOREIGN_KEY_CHECKS=0;\n\n", true)
+}
+
+func writeToFile(name string, content string, append bool)  {
+	var fileObj *os.File
+	var err error
+	if append{
+		fileObj, err = os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	}else{
+		fileObj, err = os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	}
+	if err != nil {
+		fmt.Println("Failed to open the file", err.Error())
+		os.Exit(2)
+	}
+	defer fileObj.Close()
 	if _, err := fileObj.WriteString(content); err == nil {
-		fmt.Println("Successful writing to the file with .")
+		fmt.Println("Successful writing to the file.")
 	}
 }
