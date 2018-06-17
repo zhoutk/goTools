75339eecdba1263821db7bf3090fab2d57c9e1ad
zhoutk <zhoutk@189.cn>
2018-06-17 17:27:05 +0800

    file alias.

--- backUp/export.go
+++ backUp/export.go
@@ -7,11 +7,11 @@ import (
 )
 
 type DbConnFields struct {
-	dbHost string
-	dbPort int
-	dbUser string
-	dbPass string
-	dbName string
+	dbHost    string
+	dbPort    int
+	dbUser    string
+	dbPass    string
+	dbName    string
 	dbCharset string
 	fileAlias string
 }
@@ -19,28 +19,28 @@ type DbConnFields struct {
 func Export() (bool, error) {
 	var configs interface{}
 	fr, err := os.Open("./configs.json")
-	if err != nil{
+	if err != nil {
 		return false, err
 	}
 	decoder := json.NewDecoder(fr)
 	err = decoder.Decode(&configs)
-	if err != nil{
+	if err != nil {
 		return false, err
 	}
 	confs := configs.(map[string]interface{})
 	workDir := confs["workDir"].(string)
-	for key, value := range confs{
+	for key, value := range confs {
 		if strings.HasPrefix(key, "db_") {
 			dbConf := value.(map[string]interface{})
 			dbConn := DbConnFields{
-				dbHost : dbConf["db_host"].(string),
-				dbPort : int(dbConf["db_port"].(float64)),
-				dbUser : dbConf["db_user"].(string),
-				dbPass : dbConf["db_pass"].(string),
-				dbName : dbConf["db_name"].(string),
-				dbCharset : dbConf["db_charset"].(string),
+				dbHost:    dbConf["db_host"].(string),
+				dbPort:    int(dbConf["db_port"].(float64)),
+				dbUser:    dbConf["db_user"].(string),
+				dbPass:    dbConf["db_pass"].(string),
+				dbName:    dbConf["db_name"].(string),
+				dbCharset: dbConf["db_charset"].(string),
 			}
-			if dbConf["file_alias"] != nil{
+			if dbConf["file_alias"] != nil {
 				dbConn.fileAlias = dbConf["file_alias"].(string)
 			}
 			ExportOne(dbConn, workDir)
--- backUp/exportOne.go
+++ backUp/exportOne.go
@@ -7,24 +7,29 @@ import (
 	"strconv"
 )
 
-func ExportOne(fields DbConnFields, workDir string)  {
-	name := workDir + fields.fileAlias + time.Now().Format("2006-01-02") + ".sql"
-	fileObj,err := os.OpenFile(name,os.O_RDWR|os.O_CREATE|os.O_TRUNC,0644)
+func ExportOne(fields DbConnFields, workDir string) {
+	var name string
+	if fields.fileAlias == "" {
+		name = workDir + fields.dbName + "-" + time.Now().Format("2006-01-02") + ".sql"
+	}else{
+		name = workDir + fields.fileAlias + "-" + time.Now().Format("2006-01-02") + ".sql"
+	}
+	fileObj, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
-		fmt.Println("Failed to open the file",err.Error())
+		fmt.Println("Failed to open the file", err.Error())
 		os.Exit(2)
 	}
 	defer fileObj.Close()
 	var content string
 	content = "/*   Mysql export" +
-		 "\n\n		Host: " + fields.dbHost +
+		"\n\n		Host: " + fields.dbHost +
 		"\n\n		Port: " + strconv.Itoa(fields.dbPort) +
 		"\n\n		DataBase: " + fields.dbName +
 		"\n\n		Date: " + time.Now().Format("2006-01-02 15:04:05") +
 		"\n\n		Author: zhoutk@189.cn" +
 		"\n\n		Copyright: tolw-2018" +
 		"\n*/\n\n"
-	if _,err := fileObj.WriteString(content);err == nil {
+	if _, err := fileObj.WriteString(content); err == nil {
 		fmt.Println("Successful writing to the file with .")
 	}
 }
